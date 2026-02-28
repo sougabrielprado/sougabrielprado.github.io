ca2f4c8068f8ecdee0e395aa0162f8ab93e8e05c
Gabriel Prado <sougabrielprado@gmail.com>
2026-02-28 09:44:10 -0300

    Delete 0_Kernel_Security directory

--- 0_Kernel_Security/anti_forensic_monitor.py
+++
@@ -1,9 +0,0 @@
-import time, os
-# Monitoramento de acesso não autorizado
-# Simula proteção FaceID/Biometria para o C-Level Dashboard
-def monitor_security():
-    while True:
-        # Se um IP desconhecido ou processo forense for detectado, apaga rastros temporários
-        # print('[+] Módulo 17: Vigilância Anti-Forense Ativa')
-        time.sleep(60)
-if __name__ == '__main__': monitor_security()
